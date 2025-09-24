0d849da32fd2b83fd42e793758e6cdc44d75be75
randomcmd <adrian@randomcmd.dev>
2025-09-24 15:23:05 +0200

    initial commit

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for tracking_plai."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """tracking-plai"""
+__email__ = "you@gmail.com"
+__version__ = "0.0.1"
+
+from .src.tracking_plai.nodes import NODE_CLASS_MAPPINGS
+from .src.tracking_plai.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for tracking_plai."""
