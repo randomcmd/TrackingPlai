5f7ec13ec95b51d10acad1739ba4135a7bdc6836
randomcmd <adrian@randomcmd.dev>
2025-09-24 15:48:42 +0200

    changed name

--- src/tracking_plai/nodes.py
+++ src/tracking_plai/nodes.py
@@ -1,5 +1,5 @@
 from inspect import cleandoc
-class Example:
+class TrackingPlai:
     """
     A example node
 
@@ -49,49 +49,22 @@ class Example:
         """
         return {
             "required": {
-                "image": ("Image", { "tooltip": "This is an image"}),
-                "int_field": ("INT", {
-                    "default": 0,
-                    "min": 0, #Minimum value
-                    "max": 4096, #Maximum value
-                    "step": 64, #Slider's step
-                    "display": "number" # Cosmetic only: display as "number" or "slider"
-                }),
-                "float_field": ("FLOAT", {
-                    "default": 1.0,
-                    "min": 0.0,
-                    "max": 10.0,
-                    "step": 0.01,
-                    "round": 0.001, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
-                    "display": "number"}),
-                "print_to_screen": (["enable", "disable"],),
-                "string_field": ("STRING", {
-                    "multiline": False, #True if you want the field to look like the one on the ClipTextEncode node
-                    "default": "Hello World!"
-                }),
+                "images": ("IMAGE",),
             },
         }
 
     RETURN_TYPES = ("IMAGE",)
-    #RETURN_NAMES = ("image_output_name",)
+    RETURN_NAMES = ("tracking_preview")
     DESCRIPTION = cleandoc(__doc__)
-    FUNCTION = "test"
+    FUNCTION = "track"
 
     #OUTPUT_NODE = False
     #OUTPUT_TOOLTIPS = ("",) # Tooltips for the output node
 
-    CATEGORY = "Example"
+    CATEGORY = "Tracking"
 
-    def test(self, image, string_field, int_field, float_field, print_to_screen):
-        if print_to_screen == "enable":
-            print(f"""Your input contains:
-                string_field aka input text: {string_field}
-                int_field: {int_field}
-                float_field: {float_field}
-            """)
-        #do some processing on the image, in this example I just invert it
-        image = 1.0 - image
-        return (image,)
+    def track(self, images):
+        return (images,)
 
     """
         The node will always be re executed if any of the inputs change but
@@ -109,10 +82,10 @@ class Example:
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "Example": Example
+    "TrackingPlai": TrackingPlai
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Example": "Example Node"
+    "TrackingPlai": "Tracking Plai"
 }
