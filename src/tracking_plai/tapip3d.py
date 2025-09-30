c8db748f9bc38fd3d5ff3e3cfcbc16b3acef7940
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-09-30 12:02:23 +0200

    skeleton of tapip3d without imports

--- src/tracking_plai/tapip3d.py
+++ src/tracking_plai/tapip3d.py
@@ -1,6 +1,9 @@
 from inspect import cleandoc
-import numpy as np
+from pathlib import Path
 import json
+
+import numpy as np
+import torch
 
 import comfy.model_management as mm
 
@@ -30,13 +33,50 @@ class Tapip3DNode:
     CATEGORY = "Tracking"
 
     def track(self, images, tracking_point_x, tracking_point_y):
-        images_np = images.cpu().numpy()
-        images_np = np.ascontiguousarray((images_np * 255).astype(np.uint8))
-        video = self.preprocess_images(images)
+        # Track video using model
+        resolution_factor = 2
+        
+        # TODO: import load_model(checkpoint) from TAPIP3D
+        # from utils.inference_utils import load_model, read_video, inference, get_grid_queries, resize_depth_bilinear
+        model = load_model("path/to/checkpoint")
+        model.to(self.device)
 
+        inference_res = (int(model.image_size[0] * np.sqrt(resolution_factor)), int(model.image_size[1] * np.sqrt(resolution_factor)))
+        model.set_image_size(inference_res)
+
+        # TODO: Missing variables
+        support_grid_size = None
+        num_threads = None
+        num_iters = None
+
+        video, depths, intrinsics, extrinsics, query_point, support_grid_size = self.prepare_inputs_node(
+            images=images, 
+            inference_res=inference_res, 
+            support_grid_size=support_grid_size,
+            num_threads=num_threads,
+            device=self.device
+        )
+
+        with torch.autocast("cuda", dtype=torch.bfloat16):
+            # TODO: import inference from TAPIP3D
+            coords, visibs = inference(
+                model=model,
+                video=video,
+                depths=depths,
+                intrinsics=intrinsics,
+                extrinsics=extrinsics,
+                query_point=query_point,
+                num_iters=num_iters,
+                grid_size=support_grid_size,
+            )
+
+        # Save results
+        coords = coords.cpu().numpy()
+        visibs = visibs.cpu().numpy()
+
+        # dummy data
         frame_amount = video.shape[1]
 
-        # Track video using model
         results = [{
             "x": tracking_point_x, 
             "y": tracking_point_y, 
@@ -56,3 +96,17 @@ class Tapip3DNode:
         images = images * 255
         
         return images.to(self.device)
+    
+    def prepare_inputs_node(self, images, inference_res, support_grid_size, num_threads, device):
+        # TODO: Implement
+        depths = None
+        intrinsics = None
+        extrinsics = None
+        query_point = None
+        support_grid_size = None
+
+        images_np = images.cpu().numpy()
+        images_np = np.ascontiguousarray((images_np * 255).astype(np.uint8))
+        video = self.preprocess_images(images)
+
+        return (video, depths, intrinsics, extrinsics, query_point, support_grid_size)
