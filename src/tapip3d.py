930d9eaeb7a6c4556abb3dbb64e9937f80568765
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-09-30 14:35:22 +0200

    fix v100

--- src/tapip3d.py
+++ src/tapip3d.py
@@ -1,8 +1,12 @@
 from inspect import cleandoc
 import json
+import sys
+import os
 
 # TODO: THIS IMPORT IS THE BIGGEST HEADACHE EVER running it works but importing it breaks everything
-from ..TAPIP3D.utils.inference_utils import inference, load_model
+tapip_path = os.path.join(os.path.dirname(__file__), "..", "TAPIP3D")  # "C:\\Users\\mailr\\Workspace\\plai\\tracking\\TrackingPlai\\TAPIP3D"
+sys.path.append(tapip_path)
+from ..TAPIP3D import inference, load_model
 
 import numpy as np
 import torch
