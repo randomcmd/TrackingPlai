d06b483a4317d1a11cc054769da674042d0b3763
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-09-30 10:34:03 +0200

    added run_gpu.sh that downloads + updates node everytime ComfyUI is started

---
+++ run_gpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ComfyUI
+
+if [ -d "custom_nodes/TrackingPlai" ]; then
+  echo "Updating TrackingPlai"
+  cd custom_nodes/TrackingPlai
+  git pull
+  cd ../..
+else
+  echo "Downloading TrackingPlai"
+  cd custom_nodes
+  git clone https://github.com/randomcmd/TrackingPlai
+  cd ..
+fi
+
+python main.py --preview-method auto --listen 0.0.0.0 --use-sage-attention
