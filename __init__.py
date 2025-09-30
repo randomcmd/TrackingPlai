8a283bc5ac5e09e412f583d7a6dfddf7d3fa955e
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-09-30 12:46:22 +0200

    !

--- __init__.py
+++ __init__.py
@@ -4,14 +4,6 @@ import os
 from pathlib import Path
 import sys
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
-
-print(f'{PROJECT_ROOT=}')
-
-if PROJECT_ROOT not in sys.path:
-    print("Appending project root to sys path")
-    sys.path.append(PROJECT_ROOT)
-
 __all__ = [
     "NODE_CLASS_MAPPINGS",
     "NODE_DISPLAY_NAME_MAPPINGS",
