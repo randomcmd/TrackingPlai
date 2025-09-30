5ae32b587b69ba58ce4cc46c95eb9595164ba7dd
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-09-30 11:20:27 +0200

    added preprocessing

--- src/tracking_plai/tapip3d.py
+++ src/tracking_plai/tapip3d.py
@@ -1,10 +1,14 @@
 from inspect import cleandoc
+import numpy as np
+
+import comfy.model_management as mm
 
 class Tapip3DNode:
     """
     Tapip3D Tracking
     """
     def __init__(self):
+        self.device = mm.get_torch_device()
         pass
 
     @classmethod
@@ -17,8 +21,8 @@ class Tapip3DNode:
             },
         }
 
-    RETURN_TYPES = ("IMAGE",)
-    RETURN_NAMES = ("tracking_preview")
+    RETURN_TYPES = ("STRING", "IMAGE",)
+    RETURN_NAMES = ("tracking_results", "tracking_preview")
     # DESCRIPTION = cleandoc(__doc__)
     FUNCTION = "track"
 
@@ -26,6 +30,19 @@ class Tapip3DNode:
 
     def track(self, images, tracking_point_x, tracking_point_y):
 
-        
+        images_np = images.cpu().numpy()
+        images_np = np.ascontiguousarray((images_np * 255).astype(np.uint8))
+        video = self.preprocess_images(images)
 
         return (images,)
+    
+    def preprocess_images(self, images):
+        # (B, H, W, C) -> (1, B, C, H, W)
+        if len(images.shape) == 4:
+            images = images.permute(0, 3, 1, 2)  # (B, C, H, W)
+            images = images.unsqueeze(0)  # (1, B, C, H, W)
+        
+        images = images.float()
+        images = images * 255
+        
+        return images.to(self.device)
